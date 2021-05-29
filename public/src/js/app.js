15025b007a34cd0adec7c18409c4448d5661389c
Muhammad Tegar Akmal <eategar@gmail.com>
2021-05-29 22:35:06 +0700

    cleanup

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,16 +1,24 @@
-var deferredPrompt = event;
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker
+
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
     .register('/sw.js')
-    .then(function() {
-        console.log('Service Worker Registered')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-
-window.addEventListener("beforeinstallprompt",function(event) {
-    console.log("beforeinstallprompt fired");
-    event.preventDefault();
-    deferredPrompt = event;
-    return false
-})
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,13 @@
-self.addEventListener('install',function(event) {
-    console.log('[Service Worker] installing Service Worker ...',event)
-})
-self.addEventListener('activate',function(event) {
-    console.log('[Service Worker] Activiting Service Worker ...',event)
-    return self.clients.claim(); 
-})
 
-self.addEventListener('fetch',function(event) {
-    console.log('[Service Worker] Fetching Something .... ',event)
-})
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
