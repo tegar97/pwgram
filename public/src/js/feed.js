789ef91bbb6e38a3cb414fa770c5877896122fec
Muhammad Tegar Akmal <eategar@gmail.com>
2021-05-30 16:49:33 +0700

    cache on demand

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -29,7 +29,16 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
-
+function onSaveButtonClicked(event) {
+  console.log('Boom')
+  if('caches' in window) {
+    caches.open('user-requested')
+      .then(function(cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
 function createCard() {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
@@ -47,6 +56,10 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  var cardSaveButton = document.createElement('button')
+  cardSaveButton.textContent = "Save"
+  cardSaveButton.addEventListener('click',onSaveButtonClicked)
+  cardSupportingText.appendChild(cardSaveButton)
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
