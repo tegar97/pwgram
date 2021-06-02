da2507aced02cc06b51341a411e4dd151586d673
Muhammad Tegar Akmal <eategar@gmail.com>
2021-06-02 10:11:51 +0700

    fix bug

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,7 +4,10 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
-  createPostArea.style.display = 'block';
+  // createPostArea.style.display = 'block';
+  // setTimeout(function() {
+    createPostArea.style.transform = 'translateY(0)';
+  // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
@@ -20,20 +23,30 @@ function openCreatePostModal() {
 
     deferredPrompt = null;
   }
+
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker.getRegistrations()
+  //     .then(function(registrations) {
+  //       for (var i = 0; i < registrations.length; i++) {
+  //         registrations[i].unregister();
+  //       }
+  //     })
+  // }
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = 'none';
+  createPostArea.style.transform = 'translateY(100vh)';
+  // createPostArea.style.display = 'none';
 }
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
-//function for save asset to cache 
+// Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log('Boom')
-  if('caches' in window) {
+  console.log('clicked');
+  if ('caches' in window) {
     caches.open('user-requested')
       .then(function(cache) {
         cache.add('https://httpbin.org/get');
@@ -44,18 +57,20 @@ function onSaveButtonClicked(event) {
 
 function clearCards() {
   while(sharedMomentsArea.hasChildNodes()) {
-    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild)
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
   }
 }
+
 function createCard(data) {
   var cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = `url(${data.image})`;
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
   cardTitle.style.backgroundSize = 'cover';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
   cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
@@ -63,47 +78,45 @@ function createCard(data) {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = data.location;
   cardSupportingText.style.textAlign = 'center';
-  // var cardSaveButton = document.createElement('button')
-  // cardSaveButton.textContent = "Save"
-  // cardSaveButton.addEventListener('click',onSaveButtonClicked)
-  // cardSupportingText.appendChild(cardSaveButton)
+  // var cardSaveButton = document.createElement('button');
+  // cardSaveButton.textContent = 'Save';
+  // cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  // cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
 function updateUI(data) {
-  for(var i = 0 ;  i <data.length; i++) {
+  clearCards();
+  for (var i = 0; i < data.length; i++) {
     createCard(data[i]);
   }
 }
 
-var url = "https://pwgram-9b0dc-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json"
-var networkDataRecived = false
+var url = 'https://pwgram-9b0dc-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json';
+var networkDataReceived = false;
+
 fetch(url)
   .then(function(res) {
-    if(res) {
-      return res.json();
-    }
+    return res.json();
   })
   .then(function(data) {
-    networkDataRecived = true
-    var dataArray = []
-
-    for(var key in data) {
-      dataArray.push(data[key])
+    networkDataReceived = true;
+    console.log('From web', data);
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
     }
     updateUI(dataArray);
   });
 
-
-if('indexedDB' in window) {
+if ('indexedDB' in window) {
   readAllData('posts')
-  .then(function(data) {
-    if(!networkDataRecived) {
-      console.log('From Cache',data);
-      updateUI(data)
-    }
-  });
+    .then(function(data) {
+      if (!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
 }
-
